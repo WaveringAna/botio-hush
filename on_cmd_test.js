979b4d48da2d082aa8e2db994bda660686a63574
Aayan L <aayanzillis@gmail.com>
2017-04-24 18:55:33 -0400

    Initial Commit

---
+++ on_cmd_test.js
@@ -0,0 +1,27 @@
+var botio = require(process.env['BOTIO_MODULE']);
+var shell = require('shelljs');
+var now = require("performance-now")
+
+/*echo('These are the files in your repo:');
+ls().forEach(function(file) {
+  echo('  '+file);
+});
+
+botio.message('#### Hello world');
+botio.message('Your Botio installation works! View full output for the list of files in this repo');*/
+var a = now();
+shell.echo("Building: " + a);
+
+shell.config.silent = true;
+
+shell.exec('./zcutil/build.sh -j 2');
+var b = now();
+
+shell.echo("Done building: " + (b - a) / 1000 + " seconds");
+shell.echo("Testing");
+
+var c = now();
+shell.exec('./src/zcash-gtest', {silent: false});
+var d = now();
+
+shell.echo("Done Testing: " + (d - c) / 1000 + " seconds");
